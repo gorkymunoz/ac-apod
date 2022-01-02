fbd1d5d8a4af1081001e8e0954cd56b567224fc4
Gorky Muñoz <gorkymunoz@hotmail.com>
2022-01-02 11:37:07 -0500

    Add fragments to navigation home

---
+++ app/src/main/java/com/gorkymunoz/ac/apod/ui/home/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.gorkymunoz.ac.apod.ui.home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/gorkymunoz/ac/apod/ui/library/LibraryViewModel.kt
@@ -0,0 +1,7 @@
+package com.gorkymunoz.ac.apod.ui.library
+
+import androidx.lifecycle.ViewModel
+
+class LibraryViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
