e87c7637f67dd34000d2d2c8ba6cf504680f80fb
Gorky Muñoz <gorkymunoz@hotmail.com>
2022-01-19 17:00:18 -0500

    Show apod in home fragment

--- app/src/main/java/com/gorkymunoz/ac/apod/ui/home/HomeFragment.kt
+++ app/src/main/java/com/gorkymunoz/ac/apod/ui/home/HomeFragment.kt
@@ -1,32 +1,107 @@
 package com.gorkymunoz.ac.apod.ui.home
 
-import androidx.lifecycle.ViewModelProvider
+import android.graphics.Bitmap
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.gorkymunoz.ac.apod.R
+import androidx.core.view.isVisible
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.palette.graphics.Palette
+import androidx.palette.graphics.Target
+import androidx.palette.graphics.get
+import com.gorkymunoz.ac.apod.APODApp
+import com.gorkymunoz.ac.apod.data.database.source.APODRoomDataSource
+import com.gorkymunoz.ac.apod.data.repository.APODRepository
+import com.gorkymunoz.ac.apod.data.server.APODRetrofitClient
+import com.gorkymunoz.ac.apod.data.server.source.NasaAPODDataSource
+import com.gorkymunoz.ac.apod.databinding.HomeFragmentBinding
+import com.gorkymunoz.ac.apod.domain.APOD
+import com.gorkymunoz.ac.apod.framework.dispatcherprovider.StandardDispatchers
+import com.gorkymunoz.ac.apod.ui.MainViewModel
+import com.gorkymunoz.ac.apod.ui.base.BaseFragment
+import com.gorkymunoz.ac.apod.usecases.GetAPOD
+import com.gorkymunoz.ac.apod.utils.getVMFactory
+import com.gorkymunoz.ac.apod.utils.loadImageAsBitmap
 
-class HomeFragment : Fragment() {
+class HomeFragment : BaseFragment<HomeFragmentBinding>() {
 
     companion object {
         fun newInstance() = HomeFragment()
     }
 
-    private lateinit var viewModel: HomeViewModel
+    private val viewModel: HomeViewModel by viewModels {
+        getVMFactory {
+            HomeViewModel(
+                GetAPOD(
+                    APODRepository(
+                        NasaAPODDataSource(
+                            APODRetrofitClient.createService(),
+                            StandardDispatchers(),
+                            "DEMO_KEY"
+                        ),
+                        APODRoomDataSource(
+                            (getHomeActivity().applicationContext as APODApp).db,
+                            StandardDispatchers()
+                        )
+                    )
+                )
+            )
+        }
+    }
+    private val mainViewModel by activityViewModels<MainViewModel>()
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.home_fragment, container, false)
+    override fun setUpViewBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ) = HomeFragmentBinding.inflate(inflater, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.apod.observe(this) {
+            bindAPOD(it)
+        }
+        //loadImage("https://apod.nasa.gov/apod/image/2201/LeonardTail_Hattenbach_960.jpg")
+
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
-        // TODO: Use the ViewModel
+    private fun bindAPOD(apod: APOD) {
+        loadImage(apod.url)
+        binding.tvApodName.text = apod.title
+        binding.laHomeLoading.isVisible = apod.url.isEmpty()
+        binding.btnHomeLearnMore.setOnClickListener {
+            findNavController().navigate(
+                HomeFragmentDirections.actionHomeFragmentToDetailFragment2(
+                    apod.date
+                )
+            )
+        }
+    }
+
+    private fun loadImage(url: String) {
+        binding.ivHomeApod.loadImageAsBitmap(url) {
+            it?.let {
+                apodImageReady(it)
+            }
+        }
+    }
+
+    private fun apodImageReady(resource: Bitmap) {
+        val palette = Palette.from(resource).generate()
+        Log.d(
+            HomeFragment::class.java.canonicalName,
+            "targets size: ${palette.targets.size}"
+        )
+        val dominantSwatch =
+            palette[Target.VIBRANT] ?: palette[Target.MUTED]
+        dominantSwatch?.let {
+            binding.cvHomeApod.setCardBackgroundColor(it.rgb)
+            binding.tvApodName.setTextColor(it.titleTextColor)
+            binding.btnHomeLearnMore.setTextColor(it.bodyTextColor)
+        }
     }
 
 }
--- app/src/main/java/com/gorkymunoz/ac/apod/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/gorkymunoz/ac/apod/ui/home/HomeViewModel.kt
@@ -1,7 +1,27 @@
 package com.gorkymunoz.ac.apod.ui.home
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.gorkymunoz.ac.apod.domain.APOD
+import com.gorkymunoz.ac.apod.usecases.GetAPOD
+import kotlinx.coroutines.launch
 
-class HomeViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class HomeViewModel(
+    private val getAPODUseCase: GetAPOD
+) : ViewModel() {
+
+    private val _apod = MutableLiveData<APOD>()
+    val apod: LiveData<APOD>
+        get() {
+            if (_apod.value == null) getAPOD()
+            return _apod
+        }
+
+    private fun getAPOD() {
+        viewModelScope.launch {
+            _apod.value = getAPODUseCase.invoke()
+        }
+    }
 }
