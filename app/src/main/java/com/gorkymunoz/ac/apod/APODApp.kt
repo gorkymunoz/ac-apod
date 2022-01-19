813952bcb489dc0cd78635116af1ac827adb8840
Gorky Muñoz <gorkymunoz@hotmail.com>
2022-01-19 17:15:23 -0500

    Add Application to hilt graph

--- app/src/main/java/com/gorkymunoz/ac/apod/APODApp.kt
+++ app/src/main/java/com/gorkymunoz/ac/apod/APODApp.kt
@@ -3,11 +3,13 @@ package com.gorkymunoz.ac.apod
 import android.app.Application
 import androidx.room.Room
 import com.gorkymunoz.ac.apod.data.database.APODDatabase
+import dagger.hilt.android.HiltAndroidApp
 
 
 /**
  * Created by Gorky Muñoz on 2/1/2022.
  */
+@HiltAndroidApp
 class APODApp : Application() {
 
     lateinit var db: APODDatabase
